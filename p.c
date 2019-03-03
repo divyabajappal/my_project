f6286ddaec7ea08cfe0b7a7d2061416942c4ca43
Navyashree <nsnavya311@gmail.com>
2019-03-03 12:42:10 +0530

    hello

--- p.c
+++ p.c
@@ -5,9 +5,16 @@ int add()
 	r=2+3;
 	return r;
 }
+int cos(){
+	printf("cos function\n");
+}
+int sin(){
+	printf("sin function\n");
+}
 int main()
 {
 	int res;
+	cos();
 	res=add();
 	printf("%d\n",res);
 }
